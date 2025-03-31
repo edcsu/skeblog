17bbe7171b3214d4b729ff4b271fe4c6110a6bff
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-03-31 16:07:26 +0300

    style: add all posts content

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,25 @@
+import AllPosts from "../../components/posts/allposts"
+
+const posts = [
+    {
+        slug : "getting-started-with-nextjs",
+        title : "Getting Started with Next JS",
+        image : "getting-started-with-nextjs.png",
+        excerpt : "Next JS is the best full stack React framework",
+        date : "2025-03-31",
+    },
+    {
+        slug : "nextjs-file-based-routing",
+        title : "Next JS file based routing",
+        image : "nextjs-file-based-routing.png",
+        excerpt : "File based routing in Next JS makes it seamless to create SPA's",
+        date : "2025-02-25",
+    }
+]
+
 const AllPostsPage = () => {
     return (
-        <>
-            <h1>All posts Page</h1>
-        </>
+        <AllPosts posts={posts} />
     )
 }
 
