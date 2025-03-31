229750483610d7c969eb7eb4eda6f08cc1f9e7ca
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-03-31 14:33:38 +0300

    feat: add core pages

---
+++ pages/posts/[slug].js
@@ -0,0 +1,9 @@
+const PostDetailsPage = () => {
+    return (
+        <>
+            <h1>Post Details Page</h1>
+        </>
+    )
+}
+
+export default PostDetailsPage
---
+++ pages/posts/index.js
@@ -0,0 +1,9 @@
+const AllPostsPage = () => {
+    return (
+        <>
+            <h1>All posts Page</h1>
+        </>
+    )
+}
+
+export default AllPostsPage
