7997f2b734dac2d7a5ea649295b28b201995566e
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-04-01 16:32:33 +0300

    style: port notifications

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,7 +1,7 @@
 import classes from './notification.module.css';
 import NotificationContext from '../../store/notificationcontext';
 import { useContext } from 'react';
-
+import ReactDOM from 'react-dom'
 function Notification(props) {
   const { title, message, status } = props;
   const { hideNotification } = useContext(NotificationContext)
@@ -22,12 +22,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal((
     <div className={cssClasses} onClick={hideNotification}>
       <h2>{title}</h2>
       <p>{message}</p>
     </div>
-  );
+  ), document.getElementById('notifications'));
 }
 
 export default Notification;
